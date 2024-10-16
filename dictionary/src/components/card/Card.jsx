5d25899e25172389220b183759fd04c656829e2c
SKR-Anna <anutich19@gmail.com>
2024-10-16 14:17:48 +0300

    установила router dom, добавила рендеринг карточек

--- dictionary/src/components/card/Card.jsx
+++ dictionary/src/components/card/Card.jsx
@@ -1,12 +1,29 @@
-//импортирую классы
+import { useState } from "react"
+import Button from "../button/Button"
 import "./Card.css"
 
 function Card({ id, english, transcription, russian }) {
+    const [turned, setTurned] = useState(false)
+
+    const handleTurnedState = () => {
+        setTurned(!turned)
+    }
+
     return (
-        <div id={id} className="word-card">
-            <h2 className="word-card h2">{english}</h2>
-            <p className="word-card p"> {transcription}</p>
-            <p className="word-card p">{russian}</p>
+        < div id={id} className="word-card" >
+            {turned ? (
+                <>
+                    <h2 className="word-card h2">{english}</h2>
+                    <p className="word-card p"> {transcription}</p>
+                    <p className="word-card p">{russian}</p>
+                </>
+            ) : (
+                <>
+                    <h2 className="word-card h2">{english}</h2>
+                    <p className="word-card p"> {transcription}</p>
+                    <Button name="Показать перевод" onClick={handleTurnedState} />
+                </>
+            )}
         </div>
     )
 }
