1a48c335fa80fa000b64c9abaaa5752355933736
SKR-Anna <anutich19@gmail.com>
2024-10-24 00:07:47 +0300

    мои попытки (безуспешные) создать анимацию используя useRef

--- dictionary/src/components/card/Card.jsx
+++ dictionary/src/components/card/Card.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Button from "../button/Button"
 import "./Card.css"
+// import { useRef } from "react"
 
 function Card({ id, english, transcription, russian }) {
     const [turned, setTurned] = useState(false)
@@ -9,20 +10,40 @@ function Card({ id, english, transcription, russian }) {
         setTurned(!turned)
     }
 
+    // вариант без ошибок в консоли, но кнопка не работает
+    // const cardRef = useRef(null);
+    // const handleTurnedState = () => {
+    //     cardRef.current.addEventListener(
+    //         'transitioned', () => {
+    //             cardRef.current.classList.toggle('turn');
+    //         },
+    //         { once: true },
+    //     );
+    // };
+
+    // консоль выдает ошибку, но карточки крутятся
+    // const cardRef = useRef(null);
+    // const handleTurnedState = () => {
+    //     cardRef.current.classList.toggle('turn')(
+    //         { once: true },
+    //     );
+    // };
+
     return (
         < div id={id} className="word-card" >
             <div className={`card-inner ${turned ? 'turn' : ''}`}>
+                {/* <div ref={cardRef} className="card-inner"> */}
                 <div className="card-front">
                     <h2 className="word-card h2">{english}</h2>
                     <p className="word-card p"> {transcription}</p>
-                    <p className="word-card p">{russian}</p>
-                    <Button name="Скрыть перевод" onClick={handleTurnedState} />
+                    <Button name="Показать перевод" onClick={handleTurnedState} />
                 </div>
 
                 <div className="card-back">
                     <h2 className="word-card h2">{english}</h2>
                     <p className="word-card p"> {transcription}</p>
-                    <Button name="Показать перевод" onClick={handleTurnedState} />
+                    <p className="word-card p">{russian}</p>
+                    <Button name="Скрыть перевод" onClick={handleTurnedState} />
                 </div>
             </div>
         </div>
