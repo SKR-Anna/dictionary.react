8968e83f965bca1ce937bb9dfd5904adf1230b01
SKR-Anna <anutich19@gmail.com>
2024-11-10 00:57:13 +0300

    исправлен счетчик изученных слов; обнаружена новая ошибка - считает всего одно слово

--- dictionary/src/components/card/Card.jsx
+++ dictionary/src/components/card/Card.jsx
@@ -1,24 +1,27 @@
-import { useState, useEffect, useRef } from "react"
+import { useState } from "react"
 import Button from "../button/Button"
 import "./Card.css"
 
 function Card({ id, english, transcription, russian, onLearnedWord }) {
     const [turned, setTurned] = useState(false);
-    const buttonRef = useRef(null); // сохдаем реф для кнопки
+    //const buttonRef = useRef(null); // создаем реф для кнопки
+
+    const [isLearned, setIsLearned] = useState(false); // состояние для отслеживания перевода
 
     const handleTurnedState = () => {
         setTurned(!turned);
-        if (!turned) { // Увеличиваем счетчик только при показе перевода
+        if (!turned && !isLearned) { // Увеличиваем счетчик только при первом переводе
             onLearnedWord(); // Вызываем функцию для увеличения счетчика (функция пришла из родительского компонента - кард слайдер)
+            setIsLearned(true); // Отмечаем, что слово было переведено
         }
     };
 
-    useEffect(() => {
-        // Устанавливаем фокус на кнопку, когда компонент монтируется
-        if (buttonRef.current) {
-            buttonRef.current.focus();
-        }
-    }, []); // Пустой массив зависимостей означает, что этот эффект выполнится только один раз после первого рендера
+    // useEffect(() => { не используем больше
+    //     // Устанавливаем фокус на кнопку, когда компонент монтируется
+    //     if (buttonRef.current) {
+    //         buttonRef.current.focus();
+    //     }
+    // }, []); // Пустой массив зависимостей означает, что этот эффект выполнится только один раз после первого рендера
 
     // вариант без ошибок в консоли, но кнопка не работает
     // const cardRef = useRef(null);
@@ -46,7 +49,7 @@ function Card({ id, english, transcription, russian, onLearnedWord }) {
                 <div className="card-front">
                     <h2 className="word-card h2">{english}</h2>
                     <p className="word-card p"> {transcription}</p>
-                    <Button name="Показать перевод" onClick={handleTurnedState} ref={buttonRef} />
+                    <Button name="Показать перевод" onClick={handleTurnedState} />
                     {/* применили реф к кнопке */}
                 </div>
 
