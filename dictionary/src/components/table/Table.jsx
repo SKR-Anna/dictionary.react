a701ea878e0fd741ea55e1c1b418617fb6911cd3
SKR-Anna <anutich19@gmail.com>
2024-11-16 16:08:24 +0300

    намудрила с кнопками

--- dictionary/src/components/table/Table.jsx
+++ dictionary/src/components/table/Table.jsx
@@ -9,6 +9,8 @@ export default function DictionaryTable() {
     const [editIndex, setEditIndex] = useState(null);
     const [formData, setFormData] = useState({ english: '', transcription: '', russian: '' });
     const [errors, setErrors] = useState({ english: false, transcription: false, russian: false });
+    const [words, setWords] = useState(data); // храним данные в состоянии, это поможет потом переключться на api, я надеюсь
+    const [currentWord, setCurrentWord] = useState({ english: '', transcription: '', russian: '' }); //состояние для отслеживания текущих значений таблицы, чтобы DictionaryTable сохранял текущее состояние редактируемых полей, а не полагался на defaultValue
 
     const handleEdit = (index) => {
         // console.log("clicked")
@@ -17,24 +19,37 @@ export default function DictionaryTable() {
             english: data[index].english,
             transcription: data[index].transcription,
             russian: data[index].russian
-        });
+        });//!!!!!!!!!!!!!!!!!!!!!!
+        setCurrentWord(words[index]);
+        console.log(currentWord);
     };
 
     const handleCancel = () => {
         setEditIndex(null);
     };
 
-    const handleSave = (index) => {
+    const handleSave = (index, updatedWord) => {
+        const newWords = words.map((word, i) => (i === index ? updatedWord : word));
+        setWords(newWords);
+        setEditIndex(null);
         // Здесь должна быть логика для сохранения изменений
         // Например, можно использовать API или обновлять локальный массив данных
         console.log(`Saving changes for word ${index}`);
         setEditIndex(null);
     };
 
+    const handleDelete = (index) => {
+        const newData = data.filter((_, i) => i !== index);
+        // Задаю обновленное состояние для массива слов, если данные хранятся в state
+        console.log(`Deleted word at index ${index}`);
+        console.log(newData);
+    };
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
         setErrors({ ...errors, [name]: !value });// Проверяем, пустое ли поле
+        setCurrentWord((prev) => ({ ...prev, [name]: value }));
     };
 
     const isFormValid = () => {
@@ -88,7 +103,7 @@ export default function DictionaryTable() {
                                 <td>{word.russian}</td>
                                 <td>
                                     <Button name="Изменить" onClick={() => handleEdit(index)} />
-                                    <Button name="Удалить" />
+                                    <Button name="Удалить" onClick={() => handleDelete(index)} />
                                 </td>
                             </>
                         )}
